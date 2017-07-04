88ff3ca38d1eaafb3b6c40fcc5b5e8a3d0cadc5c
Mstiekema <merstiekema@gmail.com>
2017-07-04 02:16:18 +0200

    Created basic info page
    Will improve it in the future

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
-var app = require('express')();
+var express = require('express')
+var app = express();
 var reqst = require('request');
 var conf = require("./config.js");
 var user = require("./user.js");
+
+// Basic website stuff
+app.use(express.static('.'));
+app.set('view engine', 'html');
 
 // User API
 user.followage(app, conf, reqst);
@@ -9,10 +14,8 @@ user.followsince(app, conf, reqst);
 user.age(app, conf, reqst);
 user.id(app, conf, reqst)
 
-var test = "Testing var"
-
 app.get('/', function(req, res) {
-  res.send(test);
+  res.render('index.html');
 });
 
 app.listen(5000, function () {
